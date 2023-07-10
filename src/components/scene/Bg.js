220dc23f2ecfebcb00f3ba543a0246092b0ae4b3
GxbrielTxrres <gtorres2204@outlook.com>
2023-07-09 23:19:54 -0500

    s'

--- src/components/scene/Bg.js
+++
@@ -1,3 +0,0 @@
-export default function Bg() {
-	return <Background path="./hdrs/1.hdr" color="black" bg tl={tl} />;
-}
--- utils/shaders/parse/vertex_main.glsl
+++ utils/shaders/parse/vertex_main.glsl
@@ -1,4 +1,4 @@
 
-    transformed = rotate(transformed, vec3(0.0,1.0,0.0), u_progress*3.14*3.);
-    // transformed += aRandom*(0.5 * sin(u_progress * 2.))*normalize(normal);
     transformed += u_progress*aRandom*normal;
+    transformed = rotate(transformed, vec3(0.0,1.0,0.0), aRandom*u_progress*3.14*3.);
+    // transformed += aRandom*(0.5 * sin(u_progress * 2.))*normalize(normal);
